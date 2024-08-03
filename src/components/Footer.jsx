78c5812853a5503bcc339ba50847c340f1c305cf
Jacksparrowpro <70373238+internetfiles@users.noreply.github.com>
2024-08-03 14:53:56 +0300

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,13 +8,9 @@ export default function Footer() {
             <div style={{display: "flex", fontSize: "1.1rem", fontWeight: "bold", gap: "3px"}}><p>Welcome to</p>
                 <div id="footer-logo"><img src="/images/logo.svg" style={{width: "90px"}} alt="Logo"/></div>
             </div>
-            <div style={{fontSize: "0.875rem"}}><p>This site does not store any files on our server, we only link to the media which is hosted on 3rd party services.</p></div>
-            <div style={{fontSize: "0.875rem"}}>Copyright © zmov 2024</div>
-        </div>
-        <div id="footer-right">
-            <Link to="/dmca" id="dmca">DMCA</Link>
-            <a target="_blank" href="https://github.com/coen-h" rel="noopener noreferrer"><i className="fa-brands fa-github" style={{color: "#ffffff"}} id="github" alt="Github Icon"></i></a>
-        </div>
+            
     </div>
     )
 }
+
+
