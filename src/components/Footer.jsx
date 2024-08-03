ead0d2827a9cb1f440d045d696e750954aa2c814
Jacksparrowpro <70373238+internetfiles@users.noreply.github.com>
2024-08-03 14:56:01 +0300

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,9 +8,13 @@ export default function Footer() {
             <div style={{display: "flex", fontSize: "1.1rem", fontWeight: "bold", gap: "3px"}}><p>Welcome to</p>
                 <div id="footer-logo"><img src="/images/logo.svg" style={{width: "90px"}} alt="Logo"/></div>
             </div>
-            
+            <div style={{fontSize: "0.875rem"}}><p></p></div>
+            <div style={{fontSize: "0.875rem"}}></div>
+        </div>
+        <div id="footer-right">
+          
+        </div>
     </div>
     )
 }
-
 
